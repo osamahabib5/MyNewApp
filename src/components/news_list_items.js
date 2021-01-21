e10466056820d1b203a67b7f492b072d41d49c0b
osamahabib5 <osama.hhabib@khi.iba.edu.pk>
2021-01-21 09:34:09 +0500

    First commit

--- src/components/news_list_items.js
+++ src/components/news_list_items.js
@@ -1,22 +1,14 @@
 import React from "react";
-import {css} from 'glamor';
+import classes from '../css/styles.css';
 
-const NewsItem = ({item}) =>{   //destructuring, we are bringing only item from props
-    
-    let news_item = css({
-        padding: '20px',
-        boxSizing: 'border-box',
-        borderBottom: '1px solid grey'
-    })
-    return(
-        <div {...news_item}>  
-        {/* We are passing class in above code. Whatever is in the
-        mentioned name, please pass all these elements into
-        the div. This is ESX */}
-            <h3>{item.title}</h3>
-            <div>{item.feed}</div>
+const NewsItem = ({ item }) => { //destructuring, we are bringing only item from props
+
+    return ( 
+        <div>
+        <h3> { item.title } </h3>  
+        <div> { item.feed } </div>  
         </div>
     )
-} 
+}
 
 export default NewsItem;
